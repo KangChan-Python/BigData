7531d104655dc7c2fabf515fb7c06bc2637da83c
kang <kch961203@gmail.com>
2021-01-28 01:43:24 +0900

    1/27 KB증권 리서치 보고서 및 추천 종목 한경 컨센시스에서 최근 한달 리서치 가져오기

---
+++ scrapysplash/scrapysplash/spiders/.ipynb_checkpoints/SplashSpider-checkpoint.py
@@ -0,0 +1,24 @@
+
+
+from scrapy.spiders import Spider
+from scrapy_splash import SplashRequest
+
+
+class MySpider(Spider):
+    name = 'splashspider'
+
+    # 1. SCRAPING
+    def start_requests(self):
+        yield SplashRequest(
+            url ='http://quotes.toscrape.com/js',
+            callback = self.parse
+        )
+
+    def parse(self, response):
+        for quote in response.css('div.quote'):
+            yield {
+                'text' : quote.css("span.text::text").extract_first(),
+                'author' : quote.css("small.author::text").extract_first(),
+                'tags': quote.css("div.tags > a.tag::text").extract(),
+            
+            }
